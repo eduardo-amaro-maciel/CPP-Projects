9baa953dfbd1d6776e6a540e9e7b12a5185c2069
Eduardo Amaro Maciel <eduardo.maciel@edu.univali.br>
2021-03-14 16:48:35 -0300

    Revert "Revert "Add files via upload""
    This reverts commit 25e003438e016d096773c49db7c1ca0f5f59920f.

---
+++ Trabalho Campo Minado - C++/nomes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "nomes.h"
+#include <windows.h>
+using namespace std;
+
+void nomes()
+{
+    cout << "\t************************************" << endl;
+    cout << "\t**      CAMPO MINADO 5 X 5        **" << endl;
+    cout << "\t**              by:               **" << endl;
+    cout << "\t**      Eduardo Amaro Maciel      **" << endl;
+    cout << "\t**     Pamela Bandeira Gerber     **" << endl;
+    cout << "\t**   Gabriel Maffei de Almeida    **" << endl;
+    cout << "\t************************************" << endl << endl;
+    Sleep(4000);
+    system("cls");
+}
---
+++ Trabalho Campo Minado - C++/nomes.h
@@ -0,0 +1,6 @@
+#ifndef NOMES_H
+#define NOMES_H
+
+    void nomes();
+
+#endif // NOMES_H
