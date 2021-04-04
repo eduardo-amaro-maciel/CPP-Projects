92fc7315dab7257c7927c28f8407babc2ff03838
Eduardo Amaro Maciel <eduardo.maciel@edu.univali.br>
2021-04-03 21:14:29 -0300

    Alterações de otimização

--- Sistemas Operacionais/aula.cpp
+++ Sistemas Operacionais/aula.cpp
@@ -3,51 +3,28 @@
 /* Dependendo da forma que for arquitetado poderá     */
 /* gerar alguns erros e problemas durante a execução  */
 
-/* By: Eduardo Amaro Maciel			      */
-/* Date: 29/03/2021				      */
-/* Email: eduardo.am@protonmail.com		      */
-/* Disciplina: Sistemas Operacionais		      */
+/* By: Eduardo Amaro Maciel							  */
+/* Date: 29/03/2021									  */
+/* Email: eduardo.am@protonmail.com				      */
+/* Disciplina: Sistemas Operacionais			      */
 
 
 #include <iostream>
 #include <thread>
-#include <chrono>
 #include <atomic>
-#include <time.h>
-#include <stdlib.h>
+#include <mutex>
 
 
 using namespace std;
-using namespace std::chrono;
 
-
-std::atomic<int> transacoes = 0;
+std::atomic <int> transacoes = 0;
 int saldo = 0;
 const int total = 5000;
+std::mutex m1;
 
 
-void Thread1(int n)
+void Thread1() 
 {
-	/* Cria a thread 1 para fazer a multiprogramação	*/
-	/* :param n: determinar um tempo de 'pausa' da thread	*/
-
-
-	// std::this_thread::sleep_for(std::chrono::seconds(n));
-	while (transacoes < total)
-	{
-		transacoes++;
-		saldo += 10;
-	}
-}
-
-
-void Thread2(int n)
-{
-	/* Cria a thread 2 para fazer a multiprogramação		*/
-	/* :param n: determinar um tempo de 'pausa'	da thread	*/
-
-
-	// std::this_thread::sleep_for(std::chrono::seconds(n));
 	while (transacoes < total)
 	{
 		transacoes++;
@@ -58,16 +35,12 @@ void Thread2(int n)
 
 int main()
 {
-	//while (transacoes < total) {
+	std::thread primeira(Thread1);		/* Criar a 1º thread */
+	std::thread segunda(Thread1);		/* Criar a 2º thread */
 
-	std::thread primeira(Thread1, 5);		/* Criar a 1º thread */
-	std::thread segunda(Thread2, 10);		/* Criar a 2º thread */
-
-	// std::cout << "threads rodando em paralelo!" << std::endl;
 	primeira.join();
 	segunda.join();
 
-	// std::cout << "threads finalizaram seus trabalhos!" << std::endl;
 	std::cout << "Saldo: " << saldo << std::endl;
-	std::cout << "Transações: " << transacoes << std::endl;
+	std::cout << "Transacoes: " << transacoes << std::endl;
 }
